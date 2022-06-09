ce709aa75481aae2cbe4218b3289d934f44e1426
Arnaldo Neto <costanetoow@gmail.com>
2022-06-09 20:23:29 -0300

    added cache decorator copy

--- app/dist/js/decorators/dom-injector.js
+++ app/dist/js/decorators/dom-injector.js
@@ -1,8 +1,14 @@
 export function domInjector(selector) {
     return function (target, propertyKey) {
+        console.log(`Modifying prototype ${target.constructor.name} 
+    and adding getter to property ${propertyKey}`);
+        let element;
         const getter = function () {
-            const element = document.querySelector(selector);
-            console.log(`Searching DOM element with selector ${selector} to inject into property ${propertyKey}`);
+            if (!element) {
+                element = document.querySelector(selector);
+                console.log(`Searching DOM element with selector 
+          ${selector} to inject into property ${propertyKey}`);
+            }
             return element;
         };
         Object.defineProperty(target, propertyKey, { get: getter });
--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,10 +1,19 @@
 export function domInjector(selector: string) {
   return function (target: any, propertyKey: string) {
+    console.log(`Modifying prototype ${target.constructor.name} 
+    and adding getter to property ${propertyKey}`);
+
+    let element: HTMLElement;
+
     const getter = function () {
-      const element = document.querySelector(selector);
-      console.log(
-        `Searching DOM element with selector ${selector} to inject into property ${propertyKey}`,
-      );
+      if (!element) {
+        element = <HTMLElement>document.querySelector(selector);
+        console.log(
+          `Searching DOM element with selector 
+          ${selector} to inject into property ${propertyKey}`,
+        );
+      }
+
       return element;
     };
 
